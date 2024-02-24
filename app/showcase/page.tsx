982a7c94289b6b113fd09342c427c0a2b91e158d
pmita <panos.mita2@outlook.com>
2024-02-24 20:20:22 +0000

    chore(navigation): adding navigation routes

---
+++ app/showcase/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default async function ShowcasePage() {
+  return (
+    <main className="grid place-content-center">
+      <h1 className="font-roboto text-primary">Showcase</h1>
+      <h1 className="font-poppins text-primary">Showcase</h1>
+    </main>
+  )
+}
