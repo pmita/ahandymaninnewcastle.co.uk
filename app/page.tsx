4c3c49a46a58c793522d84e2e30a57418a536fa0
pmita <panos.mita2@outlook.com>
2024-02-24 19:01:49 +0000

    style(tailwind): configuring base tailwind colours

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to homepage</h1>
+    <main className="grid place-content-center">
+      <h1 className="text-primary">Welcome to homepage</h1>
+      <h1 className="text-secondary">Welcome to homepage</h1>
+      <h1 className="text-danger">Welcome to homepage</h1>
     </main>
   );
 }
