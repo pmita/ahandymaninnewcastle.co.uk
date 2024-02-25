ae9752d4c455aa89195b692d7963c62683d16c0c
pmita <panos.mita2@outlook.com>
2024-02-25 19:01:55 +0000

    chore(forms): adding form details under config file

---
+++ config/forms.ts
@@ -0,0 +1,85 @@
+export const contactForm = [
+  {
+    id: 1,
+    name: 'fullName',
+    type: 'text',
+    placeholder: 'Full Name',
+    validationSchema: {
+      required: "A name is required",
+      minLength: {
+        value: 3,
+        message: "Your full name must be more than 3 characters",
+      },
+      maxLength: {
+        value: 30,
+        message: "Your full name must be less than 30 characters",
+      },
+      pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        message: "Please enter a valid full name",
+      }
+    }
+  },
+  {
+    id: 2,
+    name: 'email',
+    type: 'email',
+    placeholder: 'Email',
+    validationSchema: {
+      required: "Email is required",
+      pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        message: "Invalid email address",
+      }
+    }
+  },
+  {
+    id: 3,
+    name: 'mobile',
+    type: 'number',
+    placeholder: 'Mobile Number',
+    validationSchema: {
+      required: "Mobile number is required",
+      minLength: {
+        value: 3,
+        message: "Your phone number must be more than 3 characters",
+      },
+      maxLength: {
+        value: 12,
+        message: "Your phone number must be less than 12 characters",
+      },
+      pattern: {
+        value: /^[0-9]+$/,
+        message: "Mobile phone must be a number",
+      }
+    }
+  },
+  {
+    id: 4,
+    name: 'location',
+    type: 'text',
+    placeholder: 'Location',
+    validationSchema: {
+      required: "Location is required",
+    }
+  }
+];
+
+export const contactTextAreForm =   {
+  id: 5,
+  name: 'tellUsMore',
+  type: 'text',
+  placeholder: 'Tell us more',
+  validationSchema: {
+    required: "Let us know how we can help",
+    minLength: {
+      value: 2,
+      message: "Your phone number must be more than 3 characters",
+    },
+    maxLength: {
+      value: 250,
+      message: "Your phone number must be less than 12 characters",
+    },
+  }
+}
+
