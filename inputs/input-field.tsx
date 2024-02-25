06309c0cfb927705312d2ab72ba67b61cdc86fc6
pmita <panos.mita2@outlook.com>
2024-02-25 19:05:37 +0000

    feat(forms): adding input and textarea field components

---
+++ inputs/input-field.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { IFormField } from '@/types';
+
+export default function InputField({
+  name,
+  label,
+  register,
+  validationSchema,
+  type,
+  placeholder,
+  error,
+  ...rest
+}: IFormField) : JSX.Element {
+  return (
+    <div className="w-full flex flex-col justify-center items-start gap-5">
+      {label && (
+        <label htmlFor={name}>
+          {label}
+        </label>
+      )}
+
+      <input
+        className="w-full rounded-[6px]"
+        id={name}
+        placeholder={placeholder}
+        type={type}
+        {...register(name, validationSchema)}
+        {...rest}
+      />
+
+      {error && (
+        <span className="text-primary-error">{error}</span>
+      )}
+    </div>
+  )
+}
--- types/index.tsx
+++ types/index.tsx
@@ -1,7 +1,26 @@
+// PACKAGES
+import { UseFormRegister } from "react-hook-form";
+
 // SVG
 export type SVGPropType = {
   width?: string | number;
   height?: string | number;
   fill?: string;
   onClick?: () => void;
+}
+
+// INPUTS
+export interface IFormField {
+  name: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  register: UseFormRegister<any>;
+  validationSchema?: {
+    required?: string;
+    minLength?: { value: number; message: string };
+    maxLength?: { value: number; message: string };
+    pattern?: { value: RegExp; message: string };
+  }
+  error?: string; 
 }
