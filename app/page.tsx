084599f07dae3fbdf5e57a9bc0bfc88f3cfb47ab
pmita <panos.mita2@outlook.com>
2024-02-25 10:47:21 +0000

    refactor(banners): creating a split layout

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,23 @@ import { cn } from "@/utils/helpers";
 
 export default function Home() {
   return (
-    <Banner className={cn(bannerVariants({ variant: "split", size: 'full' }))}>
-      <BannerHeader className="bg-black flex-1 flex flex-col justify-center items-start">
-        <BannerTitle className="text-xl">We <span className="text-alternate">paint</span>, we <span className="text-alternate">build</span>, and we <span className="text-alternate">fix</span> all your house related problems</BannerTitle>
-        <BannerDescription>Affordable solutions to common problems, with a unique customer experience for everyday households</BannerDescription>
-      </BannerHeader>        
-      <BannerFooter className="bg-primary flex-1 grid place-content-center">
-        <p>Footer content</p>
-      </BannerFooter>
+    <Banner className={cn(bannerVariants({ variant: "split", className: "min-h-[90vh]" }))}>
+      <section className="bg-black flex-1 flex flex-col justify-center items-center">
+        <BannerHeader>
+          <BannerTitle 
+            className="text-xl max-w-[350px] md:max-w-[500px]"
+          >
+            We <span className="text-alternate">paint</span>, we <span className="text-alternate">build</span>, we <span className="text-alternate">fix</span> all your house related problems
+          </BannerTitle>
+          <BannerDescription>Affordable solutions to common problems, with a unique customer experience for everyday households</BannerDescription>
+        </BannerHeader>        
+        <BannerFooter className="text-center text-secondary">
+          <p>Footer content</p>
+        </BannerFooter>
+      </section>
+      <section className="bg-red flex-1 flex flex-col justify-center items-start">
+        <h1>Image will go here</h1>
+      </section>
     </Banner>
   );
 }
