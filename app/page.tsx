4ba0aa42d0ddc28164fd313ccf62ea4f0937a4de
pmita <panos.mita2@outlook.com>
2024-02-25 11:14:37 +0000

    feat(banners): implementing button variants

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+// NEXT
+import Link from "next/link";
 // COMPONENTS
 import { 
   Banner,
@@ -7,23 +9,35 @@ import {
   BannerTitle, 
   bannerVariants
 } from "@/components/banner";
+import { buttonVariants } from "@/components/ui/button";
 // UTILS
 import { cn } from "@/utils/helpers";
 
 export default function Home() {
   return (
     <Banner className={cn(bannerVariants({ variant: "split", className: "min-h-[90vh]" }))}>
-      <section className="bg-black flex-1 flex flex-col justify-center items-center">
-        <BannerHeader>
+      <section className="flex-1 flex flex-col justify-center items-center">
+        <BannerHeader className="max-w-[350px] md:max-w-[500px]">
           <BannerTitle 
-            className="text-xl max-w-[350px] md:max-w-[500px]"
+            className="text-xl text-primary"
           >
             We <span className="text-alternate">paint</span>, we <span className="text-alternate">build</span>, we <span className="text-alternate">fix</span> all your house related problems
           </BannerTitle>
-          <BannerDescription>Affordable solutions to common problems, with a unique customer experience for everyday households</BannerDescription>
+          <BannerDescription className="text-primary">Affordable solutions to common problems, with a unique customer experience for everyday households</BannerDescription>
         </BannerHeader>        
-        <BannerFooter className="text-center text-secondary">
-          <p>Footer content</p>
+        <BannerFooter className="text-center text-secondary gap-5">
+          <Link 
+            href="/contact"
+            className={cn(buttonVariants({ variant: "primary" }))}
+          >
+            Get in touch
+          </Link>
+          <Link 
+            href="/contact"
+            className={cn(buttonVariants({ variant: "secondary" }))}
+          >
+            Our work
+          </Link>
         </BannerFooter>
       </section>
       <section className="bg-red flex-1 flex flex-col justify-center items-start">
