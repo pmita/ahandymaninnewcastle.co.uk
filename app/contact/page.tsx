e09fb6a4601bfabcc2951c674ffee4f93a258b6e
Panagiotis Mita <panos.mita2@outlook.com>
2024-02-25 21:43:52 +0000

    Revert "feat: NO_TICKET - Accepting user forms"

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,10 @@
-// COMPONENTS
-import { ContactForm } from "@/components/contact-form";
 
 
 export default async function ContactPage() {
   return (
     <main className="grid place-content-center">
-      <ContactForm />
+      <h1 className="font-roboto text-primary">Contact us</h1>
+      <h1 className="font-poppins text-primary">Contact us</h1>
     </main>
   )
 }
--- types/index.tsx
+++ types/index.tsx
@@ -1,42 +1,7 @@
-// PACKAGES
-import { UseFormRegister } from "react-hook-form";
-
 // SVG
 export type SVGPropType = {
   width?: string | number;
   height?: string | number;
   fill?: string;
   onClick?: () => void;
-}
-
-// FORMS
-export interface IFormField {
-  name: string;
-  label?: string;
-  type?: string;
-  placeholder?: string;
-  register: UseFormRegister<any>;
-  validationSchema?: {
-    required?: string;
-    minLength?: { value: number; message: string };
-    maxLength?: { value: number; message: string };
-    pattern?: { value: RegExp; message: string };
-  }
-  error?: string; 
-}
-
-export interface IContactForm {
-  fullName: string;
-  email: string;
-  mobile: string;
-  location: string;
-  additionalInfo: string;
-}
-
-export type ContactFormErrors = {
-  fullName: string;
-  email: string;
-  mobile: string;
-  location: string;
-  additionalInfo: string;
 }
