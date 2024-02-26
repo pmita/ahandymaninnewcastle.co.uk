d0ec14f7232f55509b0b30ffa5a2d178e9ae44d3
pmita <panos.mita2@outlook.com>
2024-02-26 19:40:22 +0000

    chore(faqs): adding faqs under config file

---
+++ config/faq.ts
@@ -0,0 +1,42 @@
+export const faq = [
+  {
+    id: 1,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+  {
+    id: 2,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+  {
+    id: 3,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+  {
+    id: 4,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+  {
+    id: 5,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+  {
+    id: 6,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+  {
+    id: 7,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+  {
+    id: 8,
+    question: 'How do I get started?',
+    answer: 'You can get started by creating an account and then logging in to your account. Once you are logged in, you can start creating your first project.'
+  },
+];
