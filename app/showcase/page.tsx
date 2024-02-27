a3862d1072c87f02e4d16c45c2739d0c922587d1
pmita <panos.mita2@outlook.com>
2024-02-27 21:57:20 +0000

    feat(content): showcase page with server components

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -1,10 +1,16 @@
 
+// PACKAGES
+import { allProjects } from "@/.contentlayer/generated";
+import { compareDesc } from "date-fns";
 
 export default async function ShowcasePage() {
+  // SERVER LAND
+  const projects = allProjects.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+
   return (
     <main className="grid place-content-center">
-      <h1 className="font-roboto text-primary">Showcase</h1>
-      <h1 className="font-poppins text-primary">Showcase</h1>
+      <h2 className="font-font font-poppins text-xl text-primary text-center">Some of our Work</h2>
+      {projects && (<h1>We have projects to showcase</h1>)}
     </main>
   )
 }
