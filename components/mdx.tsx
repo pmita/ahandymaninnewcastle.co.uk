2e028d8c1fcd1674a5f05c668e5444e67cebe961
pmita <panos.mita2@outlook.com>
2024-03-03 18:33:21 +0000

    chore: removing comments

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -114,7 +114,6 @@ const components = {
     />
   ),
   img: ({ className, alt, ...props}: React.ImgHTMLAttributes<HTMLImageElement>) => (
-    // eslint-disable-next-line @next/next/no-img-element
     <img
       className={cn(
         "rounded border",
@@ -137,7 +136,7 @@ export function Mdx({ code, className }: MDXProps) {
 
   return (
     <article className={cn(
-        "container relative max-w-3xl py-6 lg:py-10",
+        "relative max-w-3xl text-justify",
         className
       )}
     >
