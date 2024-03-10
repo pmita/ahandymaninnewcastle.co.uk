bfcf7794ba07ef5d0fc5ee38ce3abc34d76e5f08
pmita <panos.mita2@outlook.com>
2024-03-10 11:28:29 +0000

    style(contact-form): fixing responsive design on mobiles

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import { ContactForm } from "./_components/contact-form";
 
 export default async function ContactPage() {
   return (
-    <div className="flex flex-col justify-center items-stretch w-[450px] p-4 bg-secondary">
+    <div className="flex flex-col justify-center items-stretch w-[375px] sm:w-[450px] p-4 bg-secondary">
       <ContactForm />
     </div>
   )
