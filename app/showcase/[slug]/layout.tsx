b1d1f5a6e24af447c6cf297c4af75be7764672cf
pmita <panos.mita2@outlook.com>
2024-03-10 22:33:57 +0000

    style(showcase-page): making header banner responsive

--- app/showcase/[slug]/layout.tsx
+++ app/showcase/[slug]/layout.tsx
@@ -20,15 +20,15 @@ export default function CourseChapterLayout({ children, params }: CourseChapterL
   const project = allProjects.find((project) => project.slugAsParams === params.slug);
 
   return (
-    <section className="flex flex-wrap flex-row justify-center items-stretch w-full px-5">
-      <header className="container w-full min-h-[5dvh] bg-alternate flex flex-row justify-start items-center gap-5 p-5 sticky top-0 z-50">
+    <section className="container flex flex-wrap flex-row justify-center items-stretch w-full">
+      <header className="container min-h-[5dvh] bg-alternate flex flex-row justify-start items-center gap-5 p-5 sticky top-0 z-50">
         <Link 
           href='/showcase'
-          className={cn(buttonVariants({ variant: 'primary', size: "sm" }), "w-[140px]")}  
+          className={cn(buttonVariants({ variant: 'primary', size: "sm" }), "min-w-[75px]")}  
         >
           Go Back
         </Link>
-        <h2 className="font-poppins font-bold text-center text-secondary">{project?.title}</h2>
+        <h2 className="font-poppins font-bold text-secondary text-sm sm:text-base">{project?.title}</h2>
       </header>
       <main className="container p-5 self-stretch w-full">
         {children}
