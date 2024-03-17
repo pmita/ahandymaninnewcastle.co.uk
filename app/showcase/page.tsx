62f29b78ab303551199775432e14038c639f9f7e
pmita <panos.mita2@outlook.com>
2024-03-17 18:12:41 +0000

    feat(showcase-page): implementing loading.tsx ui

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -23,32 +23,30 @@ export default async function ShowcasePage() {
   return (
     <main className="container flex flex-col justify-start items-center pt-10 px-10 min-h-[90dvh]">
       <h2 className="font-font font-poppins text-xl text-primary text-center">Some of our Work</h2>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 auto-rows-[500px] justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-              {projects.map((project) => (
-                <Link href={project?._raw.flattenedPath} key={project?._raw.flattenedPath} className="w-full h-full">
-                  <Card className="duration-500 hover:scale-105 hover:shadow-xl w-full h-full">
-                    <CardHeader className="p-0 w-full h-[60%] sm:h-[70%] overflow-hidden">
-                      <Image
-                        src="/images/banner-md.jpg"
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        style={{ width: "100%", height: "auto" }}
-                        objectFit="cover"
-                        objectPosition="100% 50%"
-                        alt={'Project Image'}
-                      />
-                    </CardHeader>
-                    <CardFooter className="flex flex-col justify-start items-center h-[40%] sm:h-[30%] pt-5 gap-2.5 bg-secondary">
-                      <h3 className="text-lg text-primary font-bold font-poppins">{project?.title}</h3>
-                      <p className="text-primary font-roboto">{project?.description}</p>
-                    </CardFooter>
-                  </Card>
-                </Link>
-              ))}
-          </section>
-        </Suspense>
+        <section className="w-full grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 auto-rows-[500px] justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+            {projects.map((project) => (
+              <Link href={project?._raw.flattenedPath} key={project?._raw.flattenedPath} className="w-full h-full">
+                <Card className="duration-500 hover:scale-105 hover:shadow-xl w-full h-full">
+                  <CardHeader className="p-0 w-full h-[60%] sm:h-[70%] overflow-hidden">
+                    <Image
+                      src="/images/banner-md.jpg"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      style={{ width: "100%", height: "auto" }}
+                      objectFit="cover"
+                      objectPosition="100% 50%"
+                      alt={'Project Image'}
+                    />
+                  </CardHeader>
+                  <CardFooter className="flex flex-col justify-start items-center h-[40%] sm:h-[30%] pt-5 gap-2.5 bg-secondary">
+                    <h3 className="text-lg text-primary font-bold font-poppins">{project?.title}</h3>
+                    <p className="text-primary font-roboto">{project?.description}</p>
+                  </CardFooter>
+                </Card>
+              </Link>
+            ))}
+        </section>
     </main>
   )
 }
