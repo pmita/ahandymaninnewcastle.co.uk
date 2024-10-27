9513557d477107ada4627100ac1299148c320e26
pmita <panos.mita2@outlook.com>
2024-10-27 18:49:44 +0000

    chore: updating base color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,42 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: 'rgb(var(--primary))',
-        secondary: 'rgb(var(--secondary))',
-        alternate : 'rgb(var(--alternate))',
-        danger: 'rgb(var(--danger))',
-        muted: 'rgb(var(--muted))',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        neutral: {
+          DEFAULT: "hsl(var(--neutral))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
 			fontFamily: {
         roboto: ['var(--font-roboto)'],
