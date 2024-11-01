9ad8292723fa4ffd2cc23980567de16cc8dc4f30
pmita <panos.mita2@outlook.com>
2024-11-01 23:07:55 +0000

    refactor(folder structure): introducing actions folder

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 // SERVER ACTIONS
-import { addQueryToDB } from "../app/(store)/contact/actions";
+import { addQueryToDB } from "@/actions/db/add-query-to-db";
 // COMPONENTS
 import { Button } from "@/components/ui/button";
 import { FieldWithLabel } from "@/components/field-with-label";
