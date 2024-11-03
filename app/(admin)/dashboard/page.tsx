84b46e8fdb301e2bf5bd8628b599d80e367ef544
pmita <panos.mita2@outlook.com>
2024-11-03 22:20:28 +0000

    feat(filtering): update url based on status

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,7 +1,16 @@
-export default async function DashboardPage() {
+import { StatusFilters } from "@/components/status-filters";
+
+type SeaerchParams = {
+  [key: string]: string | undefined;
+}
+
+export default async function DashboardPage({ searchParams }: { searchParams: SeaerchParams }) {
   return (
     <section className="flex flex-col justify-center items-center gap-5">
       <h1>Dashboard</h1>
+      <section className="flex flex-row justify-between items-stretch flex-wrap">
+        <StatusFilters status={searchParams.status || 'ALL'} />
+      </section>
     </section>
   );
 }
---
+++ components/status-filters.tsx
@@ -0,0 +1,50 @@
+
+"use client"
+
+// NEXT
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+// REACT
+import { useCallback, useState } from "react"
+// COMPONENTS
+import { Button, buttonVariants } from "@/components/ui/button";
+
+const baseOptions = ['INITIAL', 'PROGRESSED', 'COMPLETED'];
+
+export const StatusFilters = ({ status }: { status: string }) => {
+  // STATE & VARIABLES
+  const [currentOption, setCurrentOption] = useState(status || 'ALL');
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  console.log(searchParams, 'searchParams');
+
+  // EVENTS
+  const updateSearchParams = useCallback((filterOption: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    baseOptions.includes(filterOption)
+      ? params.set('status', filterOption)
+      : params.set('status', 'ALL');
+
+      setCurrentOption(filterOption);
+    replace(`${pathname}?${params.toString()}`);
+  }, [replace, searchParams, pathname]);
+
+  return (
+    <div className="flex flex-wrap flex-row justify-start items-center gap-2">
+      {['ALL', ...baseOptions].map((itemStatus: string) => (
+        <Button
+          key={itemStatus}
+          className={(buttonVariants({
+            variant: (currentOption === itemStatus ? 'default' : 'secondary'),
+            size: 'lg',
+          }))}
+          onClick={() => updateSearchParams(itemStatus)}
+        >
+          {itemStatus}
+        </Button>
+      ))}
+    </div>
+  )
+}
