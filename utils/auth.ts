0f2b42fcff2b239d357639f6e582d6586db5a5da
pmita <panos.mita2@outlook.com>
2024-11-03 17:24:49 +0000

    feat(authentication): signin the user out and adding an admin navbar for now

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,10 +1,17 @@
 // FIREBASE
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import { app, auth } from '@/firebase/client-config';
+// UTILS
+import { removeAuthCookie } from "./cookies";
 
-export const signin = async (email: string, password: string) => {
+export const signUserIn = async (email: string, password: string) => {
   const response = await signInWithEmailAndPassword(auth, email, password);
   return response;
 }
+
+export const signUserOut = async () => { 
+  await signOut(getAuth(app))
+  removeAuthCookie();
+};
 
 export const getCurrentUser = () => getAuth(app).currentUser;
