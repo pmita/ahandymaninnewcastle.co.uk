c5abf04b48a381a5a509f8d6fc1d94debd6963c2
pmita <panos.mita2@outlook.com>
2024-11-18 22:01:34 +0000

    chore: helpers and types

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -5,3 +5,14 @@ import { clsx, ClassValue } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDateFromMillis = (millis: number) => {
+  return new Intl.DateTimeFormat('en-GB', {
+    dateStyle: 'medium',
+  }).format(new Date(millis))
+};
+
+export const truncate = (str: string, length: number) => {
+  if (!str || str.length <= length) return str;
+  return `${str.slice(0, length)}...`;
+};
